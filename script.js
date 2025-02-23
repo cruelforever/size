b78955f419480daf1f857f80952a470f86365c90
cruelforever <124948760+cruelforever@users.noreply.github.com>
2025-02-23 23:19:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ form.addEventListener('submit', function(event) {
     if (count1 < 3) {
       result.innerHTML = '小孩還是持續哭鬧';
     } else {
-      result.innerHTML = '小孩終於冷靜下來，恭喜你完成安撫。';
+      result.innerHTML = '小孩終於冷靜下來，<br><span style="color:green;">恭喜你完成安撫。</span>恭喜你完成安撫。';
       document.querySelector("form [type='submit']").style.display = "none";
       count1 = 0;
     }
@@ -26,7 +26,7 @@ form.addEventListener('submit', function(event) {
   else if (selectedOption === '4') {
     const outcome = Math.random();
     if (outcome < 0.5) {
-      result.innerHTML = "小孩終於冷靜下來，恭喜你完成安撫。";  
+      result.innerHTML = "小孩終於冷靜下來，<br><span style="color:green;">恭喜你完成安撫。</span>";  
       document.querySelector("form [type='submit']").style.display = "none";
     } else {
       result.innerHTML = "小孩甩開貼紙，還是繼續哭。";
@@ -34,15 +34,15 @@ form.addEventListener('submit', function(event) {
   } 
   else if (selectedOption === '3') {
     if (!momUnderstand) {    
-      result.innerHTML = '旁邊的媽媽生氣了，說你為什麼對我小孩這麼兇，就抱著小孩離開，然後在google map 上留下一顆星評價說這家的牙醫很兇。安撫失敗。';
+      result.innerHTML = '旁邊的媽媽生氣了，說你為什麼對我小孩這麼兇，就抱著小孩離開，然後在google map 上留下一顆星評價說這家的牙醫很兇。<br><span style="color:red;">安撫失敗。</span>';
       document.querySelector("form [type='submit']").style.display = "none";
     } else {  
-      result.innerHTML = '媽媽出去外面等，小孩哭一哭後發現沒用，終於冷靜下來，恭喜你完成安撫。';
+      result.innerHTML = '媽媽出去外面等，小孩哭一哭後發現沒用，終於冷靜下來，<br><span style="color:green;">恭喜你完成安撫。</span>';
       document.querySelector("form [type='submit']").style.display = "none";
     }
   } 
   else if (selectedOption === '2') {
-    result.innerHTML = '旁邊的媽媽生氣了，說你為什麼對我小孩這麼兇，就抱著小孩離開，然後在google map 上留下一顆星評價說這家的牙醫很兇。安撫失敗。';
+    result.innerHTML = '旁邊的媽媽生氣了，說你為什麼對我小孩這麼兇，就抱著小孩離開，然後在google map 上留下一顆星評價說這家的牙醫很兇。<br><span style="color:red;">安撫失敗。</span>';
     document.querySelector("form [type='submit']").style.display = "none";
   } 
   else if (selectedOption === '5') {
@@ -51,7 +51,7 @@ form.addEventListener('submit', function(event) {
     if (count5 < 3) {
       result.innerHTML = '媽媽表示能理解我們的作法，他可以配合我們的指令';
     } else {
-      result.innerHTML = '媽媽生氣地說醫生你到底要不要治療我的小孩，然後抱著小孩離開，在Google Maps上留下了一顆星評價，說這家牙醫很沒用不會看小孩。安撫失敗。';
+      result.innerHTML = '媽媽生氣地說醫生你到底要不要治療我的小孩，然後抱著小孩離開，在Google Maps上留下了一顆星評價，說這家牙醫很沒用不會看小孩。<br><span style="color:red;">安撫失敗。</span>';
       document.querySelector("form [type='submit']").style.display = "none";
       count5 = 0; // 修正這裡，原本是 count3 = 0
     }
